ca07d4ff35767f98f147138ff90090bc966d085d
Jacek Adamek <jacek.adamek@gmail.com>
2019-02-19 11:28:45 +0100

    Adding and setting theme

--- personal/preload/init.el
+++ personal/preload/init.el
@@ -1,4 +1,10 @@
-(set-frame-font "DejaVu Sans Mono-14")
+(if (string-equal (system-name) "MacMini.local")
+    (set-frame-font "DejaVu Sans Mono-15")
+    (set-frame-font "DejaVu Sans Mono-14"))
+
 (setq-default line-spacing 1)
-;(set-frame-font "Monaco 13")
+;;(set-frame-font "Monaco 13")
+(add-to-list 'load-path "~/.emacs.d/themes/")
+(add-to-list 'custom-theme-load-path "~/.emacs.d/themes")
+
 (setq prelude-theme 'sanityinc-tomorrow-bright)

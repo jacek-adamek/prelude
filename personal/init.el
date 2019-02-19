c9292951c0536e20cc2fb411f070d6fde3c2932a
Jacek Adamek <jacek.adamek@gmail.com>
2019-02-19 01:03:39 +0100

    Adding settings

---
+++ personal/init.el
@@ -0,0 +1,24 @@
+(when (version<= "26.0.50" emacs-version )
+  (global-display-line-numbers-mode))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; (setq mac-command-modifier 'meta) ;;
+;; (setq mac-option-modifier 'super) ;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(add-to-list 'default-frame-alist '(fullscreen . maximized))
+(setq prelude-auto-save nil)
+
+(global-set-key (kbd "s-<") 'avy-goto-char-in-line)
+(global-set-key (kbd "s-:") 'avy-goto-word-1)
+(global-set-key (kbd "s-;") 'avy-goto-char)
+(global-set-key (kbd "s->") 'avy-goto-line)
+
+(prelude-require-packages '(dash-at-point robe enh-ruby-mode intero))
+
+(add-hook 'js2-mode-hook (lambda () (setq js2-basic-offset 2)))
+
+(add-hook 'haskell-mode-hook 'intero-mode)
+
+(add-hook 'ruby-mode-hook 'enh-ruby-mode)
+
+(add-hook 'elixir-mode-hook 'alchemist-mode)
---
+++ personal/preload/init.el
@@ -0,0 +1,4 @@
+(set-frame-font "DejaVu Sans Mono-14")
+(setq-default line-spacing 1)
+;(set-frame-font "Monaco 13")
+(setq prelude-theme 'sanityinc-tomorrow-bright)

01bd815cb81ee7c6aa584c4da43a5ee61ea7e28c
Jacek Adamek <jacek.adamek@gmail.com>
2020-01-30 00:06:29 +0100

    Adding key bindings and a few modes

--- personal/init.el
+++ personal/init.el
@@ -14,6 +14,26 @@
 (global-set-key (kbd "s-:") 'avy-goto-word-1)
 (global-set-key (kbd "s-;") 'avy-goto-char)
 (global-set-key (kbd "s->") 'avy-goto-line)
+(global-set-key (kbd "<s-right>") 'move-end-of-line)
+(global-set-key (kbd "<s-left>") 'crux-move-beginning-of-line)
+(global-set-key (kbd "<s-up>") 'beginning-of-buffer)
+(global-set-key (kbd "<s-down>") 'end-of-buffer)
+(global-set-key (kbd "<M-s-down>") 'dumb-jump-go)
+(global-set-key (kbd "<M-s-up>") 'dumb-jump-back)
+(global-set-key (kbd "<M-s-right>") 'next-buffer)
+(global-set-key (kbd "<M-s-left>") 'previous-buffer)
+(global-set-key (kbd "s-P") 'helm-M-x)
+(global-set-key (kbd "s-H") 'dash-at-point)
+(global-set-key (kbd "H-p") 'helm-projectile-find-file)
+(global-set-key (kbd "H-P") 'helm-projectile-switch-project)
+(global-set-key (kbd "s-f") 'swiper)
+(global-set-key (kbd "s-F") 'projectile-ag)
+(global-set-key (kbd "s-n") 'neotree-toggle)
+(global-set-key (kbd "s-N") 'neotree-projectile-action)
+(global-set-key (kbd "H-.") 'projectile-toggle-between-implementation-and-test)
+(global-set-key (kbd "H-q") 'delete-other-windows)
+(global-set-key (kbd "H-x") 'keyboard-quit)
+(global-set-key (kbd "H-s") 'magit-status)
 
 (prelude-require-packages '(dash-at-point
                             robe
@@ -21,15 +41,32 @@
                             intero
                             lsp-mode
                             lsp-ui
-                            neotree))
+                            neotree
+                            rspec-mode
+                            dumb-jump))
 
-(add-hook 'js2-mode-hook (lambda () (setq js2-basic-offset 2)))
+(add-hook 'js2-mode-hook
+          (lambda ()
+            (setq js2-basic-offset 2)))
 
 (add-hook 'haskell-mode-hook 'intero-mode)
 
 (add-hook 'ruby-mode-hook 'enh-ruby-mode)
 
 (add-hook 'elixir-mode-hook 'alchemist-mode)
+
+(add-hook 'prelude-prog-mode-hook
+          (lambda ()
+            (guru-mode -1)) t)
+
+(add-hook 'rspec-mode-hook
+          (lambda ()
+            (local-set-key (kbd "H-T") 'rspec-verify)
+            (local-set-key (kbd "H-R") 'rspec-verify-single)
+            (local-set-key (kbd "H-M") 'rspec-verify-method)))
+
+(setq dumb-jump-selector 'helm)
+(dumb-jump-mode)
 
 (setq neo-window-width 40)
 (setq neo-window-fixed-size nil)

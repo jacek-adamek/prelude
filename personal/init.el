9cdb7cec7f08ee232050c62172b85036bf71f317
Jacek Adamek <jacek.adamek@gmail.com>
2020-01-31 23:58:31 +0100

    Using magit push and pull popups

--- personal/init.el
+++ personal/init.el
@@ -62,8 +62,8 @@
 (define-key global-map (kbd "H-m") 'H-magit-global-prefix)
 (fset 'H-magit-global-prefix H-magit-map)
 
-(define-key H-magit-map "p" 'magit-pull-from-pushremote)
-(define-key H-magit-map "P" 'magit-push-current)
+(define-key H-magit-map "p" 'magit-pull-popup)
+(define-key H-magit-map "P" 'magit-push-popup)
 (define-key H-magit-map "b" 'magit-branch-popup)
 (define-key H-magit-map "s" 'magit-stash-popup)
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
